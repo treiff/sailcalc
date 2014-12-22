212a92192e7e8a112808ebe0e96f0dc775b1a268
Trevor Reiff <trevorreiff@gmail.com>
2014-12-22 15:29:04 -0500

    refactor tests

--- spec/sailcalc_spec.rb
+++ spec/sailcalc_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Sailcalc do
-  describe '#dl_ratio' do
+  describe 'dl_ratio' do
     subject { Sailcalc.dl_ratio(20000, 38) }
 
     it 'calculates the displacement to length ratio' do
@@ -9,7 +9,7 @@ describe Sailcalc do
     end
   end
 
-  describe '#hull_speed' do
+  describe 'hull_speed' do
     subject { Sailcalc.hull_speed(26) }
 
     it 'calculates the hull speed of the displacement hull' do
@@ -17,7 +17,7 @@ describe Sailcalc do
     end
   end
 
-  describe '#sa_disp' do
+  describe 'sa_disp' do
     subject { Sailcalc.sa_disp(20000, 680) }
 
     it 'calculates the sail area to displacement ratio' do
@@ -25,7 +25,7 @@ describe Sailcalc do
     end
   end
 
-  describe '#len_beam' do
+  describe 'len_beam' do
     subject { Sailcalc.len_beam(40, 12) }
 
     it 'calculates the length to beam ratio' do
@@ -33,7 +33,7 @@ describe Sailcalc do
     end
   end
 
-  describe '#cap' do
+  describe 'cap' do
     subject { Sailcalc.cap(24000, 12) }
 
     it 'calculates the capsize ratio' do
@@ -41,7 +41,7 @@ describe Sailcalc do
     end
   end
 
-  describe '#bal_disp' do
+  describe 'bal_disp' do
     subject { Sailcalc.bal_disp(24000, 9000) }
 
     it 'calculates the balast to displacement ratio' do
@@ -49,7 +49,7 @@ describe Sailcalc do
     end
   end
 
-  describe '#mcr' do
+  describe 'mcr' do
     subject { Sailcalc.mcr(32500, 39, 44, 13.5) }
 
     it 'calculates the motion comfort ratio' do
