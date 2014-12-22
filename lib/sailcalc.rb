ee2b411a084ad2693c21cf8b8d07b55af336bccf
Trevor Reiff <trevorreiff@gmail.com>
2014-12-21 20:57:19 -0500

    more tweeking of spec and #dlratio

--- lib/sailcalc.rb
+++ lib/sailcalc.rb
@@ -1,7 +1,7 @@
 require "sailcalc/version"
 
 module Sailcalc
-  def dlratio(disp, lwl)
+  def self.dlratio(disp, lwl)
     longTons = disp / 2240.0
     (longTons / (0.01 * lwl)**3).round(2)
   end
--- spec/sailcalc_spec.rb
+++ spec/sailcalc_spec.rb
@@ -1,16 +1,11 @@
 require 'spec_helper'
 
 describe Sailcalc do
-  let(:class_with_my_module) {
-    Class.new do
-      include Sailcalc
-    end
-  }
-  subject { class_with_my_module.new }
-
   describe '#dlratio' do
+    subject { Sailcalc.dlratio(20000, 38) }
+
     it 'calculates the displacement to length ratio' do
-      expect(subject.dlratio(20000, 38)).to eql(162.72)
+      expect(subject).to eql(162.72)
     end
   end
 end
