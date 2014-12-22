0c0cebe3de5140841b46686b3359411361ee3bc6
Trevor Reiff <trevorreiff@gmail.com>
2014-12-22 11:27:18 -0500

    finish perliminary formula and specs

--- lib/sailcalc.rb
+++ lib/sailcalc.rb
@@ -1,8 +1,33 @@
 require "sailcalc/version"
 
 module Sailcalc
-  def self.dlratio(disp, lwl)
-    longTons = disp / 2240.0
+  def self.dl_ratio(disp, lwl)
+    longTons = disp.to_f / 2240
     (longTons / (0.01 * lwl)**3).round(2)
+  end
+
+  def self.hull_speed(lwl)
+    (1.34 * (lwl.to_f)**0.5).round(2)
+  end
+
+  def self.sa_disp(disp, sa)
+    disp_cu_feet = disp / 64
+    (sa.to_f / (disp_cu_feet**0.6666)).round(2)
+  end
+
+  def self.len_beam(loa, bmax)
+    (loa.to_f / bmax).round(2)
+  end
+
+  def self.cap(disp, bmax)
+    (bmax.to_f / (disp/64)**0.3333).round(2)
+  end
+
+  def self.bal_disp(disp, bal)
+    ((bal.to_f / disp)*100).round(2)
+  end
+
+  def self.mcr(disp, lwl, loa, bmax)
+    (disp / (0.6666*((0.7*lwl) + (0.3333*loa))*(bmax**1.3333))).round(2)
   end
 end
--- spec/sailcalc_spec.rb
+++ spec/sailcalc_spec.rb
@@ -1,11 +1,59 @@
 require 'spec_helper'
 
 describe Sailcalc do
-  describe '#dlratio' do
-    subject { Sailcalc.dlratio(20000, 38) }
+  describe '#dl_ratio' do
+    subject { Sailcalc.dl_ratio(20000, 38) }
 
     it 'calculates the displacement to length ratio' do
       expect(subject).to eql(162.72)
+    end
+  end
+
+  describe '#hull_speed' do
+    subject { Sailcalc.hull_speed(26) }
+
+    it 'calculates the hull speed of the displacement hull' do
+      expect(subject).to eql(6.83)
+    end
+  end
+
+  describe '#sa_disp' do
+    subject { Sailcalc.sa_disp(20000, 680) }
+
+    it 'calculates the sail area to displacement ratio' do
+      expect(subject).to eql(14.79)
+    end
+  end
+
+  describe '#len_beam' do
+    subject { Sailcalc.len_beam(40, 12) }
+
+    it 'calculates the length to beam ratio' do
+      expect(subject).to eql(3.33)
+    end
+  end
+
+  describe '#cap' do
+    subject { Sailcalc.cap(24000, 12) }
+
+    it 'calculates the capsize ratio' do
+      expect(subject).to eql(1.66)
+    end
+  end
+
+  describe '#bal_disp' do
+    subject { Sailcalc.bal_disp(24000, 9000) }
+
+    it 'calculates the balast to displacement ratio' do
+      expect(subject).to eql(37.5)
+    end
+  end
+
+  describe '#mcr' do
+    subject { Sailcalc.mcr(32500, 39, 44, 13.5) }
+
+    it 'calculates the motion comfort ratio' do
+      expect(subject).to eql(36.15)
     end
   end
 end
